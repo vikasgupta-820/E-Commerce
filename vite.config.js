01e0037ebf180b5f97ed8dc082fdb6aec48df902
vikasgupta-820 <vikasboyka820@gmail.com>
2024-06-29 13:13:06 +0530

    base url change

--- vite.config.js
+++ vite.config.js
@@ -44,6 +44,6 @@ const manifestForPlugIn = {
 }
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/E-Commerce/',
+  base: '/',
   plugins: [react(), VitePWA(manifestForPlugIn)],
 })
