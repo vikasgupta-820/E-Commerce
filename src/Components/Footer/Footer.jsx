8272d6026549b7f0e03f7aa3effa2c8de5cd7d77
vikasgupta-820 <vikasboyka820@gmail.com>
2024-06-29 13:03:54 +0530

    base url change

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,10 +8,10 @@ const Footer = () => {
     <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
-              <a href="https://flowbite.com/" className="flex items-center">
+              <Link to="/" className="flex items-center">
                   <img src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" className="h-8 me-3" alt="FlowBite Logo" />
                   <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">MyIndiaa</span>
-              </a>
+              </Link>
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
               <div>
@@ -51,7 +51,7 @@ const Footer = () => {
       </div>
       <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
       <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://flowbite.com/" className="hover:underline">MyIndiaa™</a>. All Rights Reserved.
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <Link href="/" className="hover:underline">MyIndiaa™</Link>. All Rights Reserved.
           </span>
           <div className="flex mt-4 sm:justify-center sm:mt-0">
               <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
--- vite.config.js
+++ vite.config.js
@@ -44,6 +44,6 @@ const manifestForPlugIn = {
 }
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/E-Commerce/',
+  // base: '/E-Commerce/',
   plugins: [react(), VitePWA(manifestForPlugIn)],
 })
