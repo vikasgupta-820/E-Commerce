fcd691eec3f7591028a3c28609b8cd1241c0a9e8
vikasgupta-820 <vikasboyka820@gmail.com>
2024-06-29 13:41:40 +0530

    updated stores url

--- src/Components/Header/NavBar/NavBar.jsx
+++ src/Components/Header/NavBar/NavBar.jsx
@@ -154,12 +154,12 @@ const NavBar = () => {
               <div className="space-y-6 border-t border-gray-200 px-4 py-6">
                 {navigation.pages.map((page) => (
                   <div key={page.name} className="flow-root">
-                    <a
-                      href={page.href}
+                    <Link
+                      to={page.href}
                       className="-m-2 block p-2 font-medium text-gray-900"
                     >
                       {page.name}
-                    </a>
+                    </Link>
                   </div>
                 ))}
               </div>
